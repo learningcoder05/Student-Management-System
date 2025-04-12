6aca81a72888526ecbfa8e5088882058339efdac
learningcoder05 <ns580654@gmail.com>
2025-04-12 23:02:33 +0530

    Student Management class left

--- studentManagement.cpp
+++ studentManagement.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 #include<vector>
 using namespace std;
+
 class Student{
     string name;
     int roll;
@@ -8,11 +9,11 @@ class Student{
     vector<string> listOfCourses;
     public:
     Student(){
-        name="john doe";
+        name="John Doe";
         roll=001;
         CGPA=9.0;
         listOfCourses={"Mathematics","FOCP"};
-        cout<<"Hello\n";
+        // cout<<"Hello\n";
     }
     Student(string n,int rn,float cg,string lC){
         name=n;
@@ -20,9 +21,132 @@ class Student{
         CGPA=cg;
         listOfCourses.push_back(lC);
     }
+    Student(Student &t){
+        name=t.name;
+        roll=t.roll+1;
+        CGPA=t.CGPA;
+        listOfCourses=t.listOfCourses;
+    }
+    void updateCGPA(){
+            cout<<"Enter updated CGPA: ";
+            cin>>CGPA;
+        
+    }
+    void displayInfo(){
+        cout<<"----------Student Records----------"<<endl;
+        cout<<"Name: "<<name<<endl;
+        cout<<"Roll No.: "<<roll<<endl;
+        cout<<"CGPA: "<<CGPA<<endl;
+        cout<<"Courses: ";
+        for(auto x:listOfCourses){
+            cout<<x<<" ";
+        }
+        cout<<endl;
+        cout<<"----------###############----------"<<endl;
+    }
+
+    
+
+    // Functionality
+    void addNewCourses(){
+        int n;
+        cout<<"Enter number of subjects to add: "<<endl;
+        cin>>n;
+        string newCourse;
+        for(int i=1;i<=n;i++){
+            if(i==1){
+            cout<<"Enter name of "<<i<<"st subject: ";
+            }
+            if(i==2){
+            cout<<"Enter name of "<<i<<"nd subject: ";
+            }
+            if(i==3){
+            cout<<"Enter name of "<<i<<"rd subject: ";
+            }
+            if(i>3){
+            cout<<"Enter name of "<<i<<"th subject: ";
+            }
+            cin>>newCourse;
+            listOfCourses.push_back(newCourse);
+        }
+    }
+    ~Student(){
+        // clears data ;
+    }
 };
+
+class basicFunctions: public Student {
+    public:
+    void menu(){
+        cout<<"----------Welcome to Student Management Student----------"<<endl;
+        cout<<"What do you want to do?"<<endl;
+        cout<<"1. Add new course"<<endl;
+        cout<<"2. Update cgpa"<<endl;
+        cout<<"3. Display Student records"<<endl;
+    }
+
+    int opt(){
+        int opt;
+        cout<<"Enter your option: ";
+        cin>>opt;
+        return opt;
+    }
+};
+
+// class StudentManagementSystem:public Student{
+//     vector<Student> stu;
+//     public:
+//     void addNewStudent(){
+//         string n;
+//         int rn;
+//         float cg;
+//         string listOfCourse;
+//         cout<<"Enter name of new student: ";
+//         cin>>n;
+//         cout<<"Enter roll no. of new student: ";
+//         cin>>rn;
+//         cout<<"Enter cgpa of new student: ";
+//         cin>>cg;
+//         cout<<"Enter courses of new student: ";
+//         cin>>listOfCourse;
+
+//         // cin>>Student::;
+//     }
+
+//     int findRollNumber(string n){
+//         int roll;
+
+//         return roll;
+//     }
+
+//     void displayAllRecords(){
+
+//     }
+
+//     void collectionOfObjects(){
+//         // vector<Student> stu;
+//     }
+// };
 int main(){
     Student student;
-    Student student("Naman",001,9.5,"");
-return 0;
+    basicFunctions b;
+    char ans='y';
+    while(ans=='y'){
+    b.menu();
+    switch (b.opt())
+    {
+    case 1:
+        student.addNewCourses();
+        break;
+    case 2:
+        student.updateCGPA();    
+        break;
+    case 3:
+        student.displayInfo();    
+    default:
+        break;
+    }
+    cout<<"Wanna continue?(y/n): ";
+    cin>>ans;
+}
 }
